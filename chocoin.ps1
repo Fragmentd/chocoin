85084bb87e1e78aa0487f7934ad60bb0359e59ce
Fragmentd <fragment85@gmail.com>
2018-10-09 01:14:58 -0400

    Update chocoin.ps1
    Chocolatey Installer Complete

--- chocoin.ps1
+++ chocoin.ps1
@@ -6,34 +6,87 @@
 === Date Sept 15, 2018
 === Disclaimer: Use at your own Risk!
 === ToDo: Please see todo.md
-
-=== Changes:
+=== Change Log:
 0.01 - Sept 15, 2018 - Script Layout.
 0.02 - Sept 21, 2018 - Added Color to Install Text.
 0.03 - Sept 22, 2018 - Added ChocolateyGUI to initial install
+0.04 - Oct 08, 2018
+    + Added:
+       - Added Variables section.
+       - Added Disclaimer Printout.
+       - Added Info Section Printout.
+       - Added User input for the questions.
+    + Changed:
+       - question format for initial install.
+       - Color Changes.
+       - More Color Changes.
+       - Reorder of Notes - Switched the positions of the  Disclaimer and Info Section.
+0.05 - Oct 09, 2018
+    + Added:
+      - Chocolatey Installer Complete
+    + Changed:
+
 === Disclaimer: Use at your own Risk!
-#>
-Write-host "Choco In - Installer" -ForegroundColor Green -BackgroundColor Black
+================================================== #>
+# Variables Section
+$VersionNumber = "0.05 Alpha"
+$ExecutionPolicyStatus = get-executionpolicy
+$chocoversion = choco --version
 
-Write-host "Would you like to Download and Install Chocolatey? (Default is No)" -ForegroundColor Red -BackgroundColor Yellow
+# This section will print the title of the scrip.
+# $VersionNumber
+Write-host "====================INFO======================" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "| Choco Favorites Installer - ChocoIn        |" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host -NoNewline "| Version Number:"             -ForegroundColor DarkGray -BackgroundColor Black
+Write-host -NoNewline $VersionNumber  -ForegroundColor Red -BackgroundColor Black
+Write-host "                  |"             -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "| Please submit change requests              |" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "| Or Feedback to:                            |" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "|          FragDProjects@gmail.com           |" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "==============================================" -ForegroundColor DarkGray -BackgroundColor Black
 
-<#    $Readhost = Read-Host " ( y / n ) "
-    Switch ($ReadHost)
-     {
-       Y {Write-host "Yes, Download Chocolatey"; $UserInput1=$true}
-       N {Write-Host "No, Exiting now..."; $UserInput1=$false}
-       Default {Write-Host "Default, Exit"; $UserInput1=$false}
-     }
-#>
+# Disclaimer Print out
+Write-host "==============================================" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "|! ! ! Disclaimer: Use at your own Risk ! ! !|" -ForegroundColor DarkGray -BackgroundColor Black
+Write-host "==============================================" -ForegroundColor DarkGray -BackgroundColor Black
 
-$confirmation = Read-Host "Do you want to continue?[Y]/[N]:"
-if ($confirmation -eq 'y') {
-  # proceed
-}
-Write-host -ForegroundColor Green -BackgroundColor Black "Chocolatey Is now being downloaded"
+# Pause
+Start-Sleep -s 3
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Write-host "! ! ! WARNING ! ! !" -ForegroundColor Red -BackgroundColor Black
+Write-host "Continuing will set Execution Policy for the entire system!"
+Write-host "Answering No will exit the script." -ForegroundColor Red -BackgroundColor Black
 
-Write-host -ForegroundColor Green -BackgroundColor Black "TEST"
-Write-host -ForegroundColor Green -BackgroundColor Black "TEST"
-Write-host -ForegroundColor Green -BackgroundColor Black "TEST"
+$confirmationexepol = Read-Host "[y|n]"
+if ($confirmationexepol -eq 'y') {
+Write-host "Bypassing Execution Policy" -ForegroundColor Green -BackgroundColor Black
+
+# Sets ExecutionPolicy Bypass, (Is this even needed? As execution policy needs to be set before this script would run).
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Retrieve the execution policy status.
+write-host "Execution Policy Status: $ExecutionPolicyStatus"
+
+# Pause
+Start-Sleep -s 1
+} else {exit}
+
+#Line Break
+Write-host "==============================================" -ForegroundColor Green -BackgroundColor Black
+
+#Chocolatey Install
+Write-host "Would you like to Download and Install Chocolatey?" -ForegroundColor Green -BackgroundColor Black
+
+$confirmationChoco = Read-Host "[y|n]"
+if ($confirmationChoco -eq 'y') {
+
+# Pause
+Start-Sleep -s 2
+
+Write-host "Chocolatey Is now being downloaded" -ForegroundColor Green -BackgroundColor Black
+
+# Downloads and Installs Chocolatey
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {exit}
+
+Write-Host "Chocolatey Installed Version is: $chocoversion"
